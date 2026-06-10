60dab459cecb6681434ad1dad3ba0e602c3d478d
Jon Mico <jonmico@gmail.com>
2026-06-09 23:23:16 -0400

    Actually creates a user now.

--- src/controllers/user/register.ts
+++ src/controllers/user/register.ts
@@ -1,14 +1,22 @@
-import type { NextFunction, Request, Response } from "express";
+import type { Request, Response } from "express";
+import pool from "../../db/connection.js";
 
-export default function register(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
+export default async function register(req: Request, res: Response) {
   // TODO: Write register controller.
   const { email, password } = req.body;
 
   console.log(email, password);
 
-  res.send({ email, password });
+  await pool.query("INSERT INTO users (email, password_hash) VALUES (?, ?)", [
+    email,
+    password,
+  ]);
+
+  const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
+    email,
+  ]);
+
+  console.log(rows);
+
+  res.send({ rows });
 }
