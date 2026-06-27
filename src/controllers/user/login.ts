312902e84c9f6b6622ec7e4f991060e4a4131c9e
Jon Mico <jonmico@gmail.com>
2026-06-27 15:03:09 -0400

    Adds login controller.

---
+++ src/controllers/user/login.ts
@@ -0,0 +1,48 @@
+import type { Request, Response } from "express";
+import pool from "../../db/connection.js";
+import type { RowDataPacket } from "mysql2";
+import * as argon2 from "argon2";
+import signToken from "../../utils/signToken.js";
+
+interface RequestBody {
+  email: string;
+  password: string;
+}
+
+interface ExistingUserRows extends RowDataPacket {
+  email: string;
+  password_hash: string;
+  id: string;
+}
+
+export default async function login(
+  req: Request<unknown, unknown, RequestBody>,
+  res: Response,
+) {
+  const { email, password } = req.body;
+
+  const [existingUserRows] = await pool.query<ExistingUserRows[]>(
+    "SELECT email, password_hash, id FROM users WHERE email = ?",
+    [email],
+  );
+
+  const existingUser = existingUserRows[0];
+
+  if (!existingUser) {
+    return res.status(404).send({ message: "User does not exist." });
+  }
+
+  const result = await argon2.verify(existingUser.password_hash, password, {
+    secret: Buffer.from(process.env.PEPPER!),
+  });
+
+  if (!result) {
+    return res.status(403).send({ message: "Incorrect password." });
+  }
+
+  const jwt = await signToken(existingUser.id);
+
+  return res
+    .cookie("jwt", jwt, { signed: true, httpOnly: true })
+    .send({ message: "Successfully logged in." });
+}
