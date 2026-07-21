180dbc3c675fb00d62cf980c05ec3bc2a3dfb749
Jon Mico <jonmico@gmail.com>
2026-07-20 21:06:28 -0400

    Updates getMoods endpoint.

--- src/controllers/moods/getMoods.ts
+++ src/controllers/moods/getMoods.ts
@@ -1,7 +1,26 @@
 import type { Request, Response } from "express";
+import type { RowDataPacket } from "mysql2";
+import pool from "../../db/connection.js";
+
+interface MoodsRows extends RowDataPacket {
+  id: string;
+  user_id: string;
+  mood: number;
+  notes: string;
+  created_at: string;
+}
 
 export async function getMoods(req: Request, res: Response) {
-  console.log(req);
+  const limit = req.query.limit ? parseInt(req.query.limit as string) : 10;
 
-  return res.json({ message: "You are in the getMoods controller!" });
+  const [moods] = await pool.query<MoodsRows[]>(
+    `SELECT id, user_id, mood, notes, created_at
+    FROM moods
+    WHERE user_id = ?
+    ORDER BY created_at DESC
+    LIMIT ?`,
+    [req.userId, limit],
+  );
+
+  return res.json({ moods });
 }
