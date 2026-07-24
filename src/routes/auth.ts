6297d295de712290d77551b3372fda03be18c548
Jon Mico <jonmico@gmail.com>
2026-07-24 06:35:38 -0400

    Forgot to click this.

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authenticate from "../middleware/authenticate.js";
+import { login, logout, me, register } from "../controllers/auth/index.js";
 
 export const userRouter = Router();
 
 userRouter.post("/register", register);
 userRouter.post("/login", login);
+userRouter.post("/logout", logout);
 userRouter.get("/me", authenticate, me);
